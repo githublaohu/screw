91e926cd63d1f1b9392ed8f1c50481072cca4938
laohu <hujie@fuzhi.ai>
2022-03-03 22:26:27 +0800

    fix(nacos,etcd):更新配置

---
+++ etcd/start.sh
@@ -0,0 +1,3 @@
+nohup /environmental/software/etcd/etcd --config-file /environmental/screw/etcd/etcd.conf > /environmental/software/etcd/etcd.log 2>&1 &
+
+#nohup /environmental/software/etcd/etcd --config-file /environmental/config/etcd/etcd.conf > /environmental/logs/etcd/etcd.log 2>&1 &
